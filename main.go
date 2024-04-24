f3470ac20cb3088e5e8bb90333c16d86aa793073
Erik <erikmillergalow@gmail.com>
2024-04-24 19:11:58 -0400

    Using Hack font. Sidebar, chat, and current thread view styling. Send message input in current thread view. Example thread in sidebar for styling

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import (
     "log"
     "os"
+    "context"
+    "net/http"
 
+    "github.com/erikmillergalow/htmx-llmchat/templates"
+
+    "github.com/labstack/echo/v5"
     "github.com/pocketbase/pocketbase"
     "github.com/pocketbase/pocketbase/apis"
     "github.com/pocketbase/pocketbase/core"
@@ -17,7 +22,26 @@ func main() {
         e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
 
         // begin endpoints
+        e.Router.GET("/threads", func(c echo.Context) error {
+            c.Response().Writer.WriteHeader(200)
 
+            entryParams := templates.ThreadListEntryParams{
+                ThreadTitle: "Test title",
+                Model: "Llama 3",
+                StartTime: 111111111,
+                LastMessageTime: 444444444,
+                Tags: []string{"TagA", "TagB"},
+                LastMessage: "This is the last message in the chat and...",
+            }
+            threadListEntry := templates.ThreadListEntry(entryParams)
+
+            err := threadListEntry.Render(context.Background(), c.Response().Writer)
+            if err != nil {
+                return c.String(http.StatusInternalServerError, "failed to render thread list repsonse")
+            }
+
+            return nil               
+        })
 
         return nil
     })
