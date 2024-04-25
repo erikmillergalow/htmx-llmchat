cef487474fa55eb6b6c7c72ceb517d3e2ce5f762
Erik <erikmillergalow@gmail.com>
2024-04-25 02:28:42 -0400

    x/net/websocket (not working)

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "fmt"
     "log"
     "os"
     "context"
@@ -9,6 +10,8 @@ import (
     "github.com/erikmillergalow/htmx-llmchat/templates"
 
     "github.com/labstack/echo/v5"
+    "golang.org/x/net/websocket"
+
     "github.com/pocketbase/pocketbase"
     "github.com/pocketbase/pocketbase/apis"
     "github.com/pocketbase/pocketbase/core"
@@ -41,6 +44,32 @@ func main() {
             }
 
             return nil               
+        })
+
+        // websocket connection:
+        e.Router.GET("/ws", func(c echo.Context) error {
+            websocket.Handler(func(ws *websocket.Conn) {
+                defer ws.Close()
+                for {
+                    // write
+                    err := websocket.Message.Send(ws, "socket message")
+                    if err != nil {
+                        fmt.Println("socket write error")
+                        fmt.Println(err)
+                        break
+                    }
+
+                    msg := ""
+                    err = websocket.Message.Receive(ws, &msg)
+                    if err != nil {
+                        fmt.Println("socket read error")
+                        fmt.Println(err)
+                        fmt.Println(msg)
+                        break 
+                    }
+                }
+            }).ServeHTTP(c.Response(), c.Request())
+            return nil
         })
 
         return nil
