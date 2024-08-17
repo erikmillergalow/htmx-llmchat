dc31ec72d0a9aec88c0acd48a777e1b6ec75c212
Erik Miller-Galow <emiller-galow@betterbin.app>
2024-08-17 02:35:59 -0400

    Run pocketbase as Tauri sidecar

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,24 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{
+  api::process::Command,
+  Manager,
+};
+
 fn main() {
   tauri::Builder::default()
+    .setup(|app| {
+        let _window = app.get_window("main").unwrap();
+        tauri::async_runtime::spawn(async move {
+            let (mut _rx, mut _child) = Command::new_sidecar("main")
+                .expect("failed to setup main sidecar")
+                .args(["serve"])
+                .spawn()
+                .expect("failed to spawn packaged pocketbase");
+        });
+        Ok(())
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
