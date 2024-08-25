23d5257da6e03dcb81101f6adf77f19db91fc13e
Erik Miller-Galow <emiller-galow@betterbin.app>
2024-08-25 03:04:10 -0400

    Chat related errors show up in chat window and are stored in the DB

--- chatSocket.go
+++ chatSocket.go
@@ -28,6 +28,37 @@ type HTMXSocketMsg struct {
 	ThreadId string            `json:"thread-id-chat"`
 }
 
+func handleChatError(err error, ws *websocket.Conn, threadId string, app *pocketbase.PocketBase) {
+	if err != nil {
+		errorMessage := fmt.Sprintf("Encountered an error: %v", err)
+
+		chatCollection, _ := app.Dao().FindCollectionByNameOrId("chat")
+		chatErrorRecord := models.NewRecord(chatCollection)
+		form := forms.NewRecordUpsert(app, chatErrorRecord)
+		form.LoadData(map[string]any{
+			"thread_id": threadId,
+			"message": errorMessage,
+			"sender": "system",
+			"model": "error",
+		})
+		if submitErr := form.Submit(); submitErr != nil {
+			fmt.Printf("failed to submit chat error message: %v\n", submitErr)
+		}
+
+		errorResponse := templates.ErrorChatResponse(errorMessage)
+		var htmlBuf bytes.Buffer
+		renderErr := errorResponse.Render(context.Background(), &htmlBuf)
+		if renderErr != nil {
+			fmt.Printf("failed to render chat error message: %v\n", renderErr)
+		}
+		htmlStr := htmlBuf.String()
+		err = ws.WriteMessage(websocket.TextMessage, []byte(htmlStr))
+		if err != nil {
+			fmt.Printf("failed to write chat error response to websocket: %v\n", renderErr)
+		}
+	}
+}
+
 func OpenChatSocket(selectedModel *string, c echo.Context, app *pocketbase.PocketBase) error {
 	fmt.Println("websocket triggered")
 	
@@ -54,7 +85,7 @@ func OpenChatSocket(selectedModel *string, c echo.Context, app *pocketbase.Pocke
 	if err != nil {
 		return err
 	}
-
+	
 	for {
 		// read
 		_, msg, err := ws.ReadMessage()
@@ -69,7 +100,8 @@ func OpenChatSocket(selectedModel *string, c echo.Context, app *pocketbase.Pocke
 		err = json.Unmarshal(msg, &htmxMsg)
 		if err != nil {
 			fmt.Printf("error parsing message: %v\n", err)
-			return err
+			handleChatError(err, ws, htmxMsg.ThreadId, app)
+			continue
 		}
 		fmt.Println(htmxMsg)
 
@@ -78,12 +110,14 @@ func OpenChatSocket(selectedModel *string, c echo.Context, app *pocketbase.Pocke
 			// fetch selected model config
 			userRecord, err := app.Dao().FindFirstRecordByData("users", "username", "default")
 			if err != nil {
-				return err
+				handleChatError(err, ws, htmxMsg.ThreadId, app)
+				continue
 			}
 
 			selectedModelRecord, err := app.Dao().FindRecordById("apis", userRecord.GetString("selected_api"))
 			if err != nil {
-				return err
+				handleChatError(err, ws, htmxMsg.ThreadId, app)
+				continue
 			}
 
 			chatModelName := selectedModelRecord.GetString("name")
@@ -104,7 +138,8 @@ func OpenChatSocket(selectedModel *string, c echo.Context, app *pocketbase.Pocke
 			})
 			if err := form.Submit(); err != nil {
 				fmt.Printf("Failed to submit user message to chat DB: %v\n", err)
-				return err
+				handleChatError(err, ws, htmxMsg.ThreadId, app)
+				continue
 			}
 
 			// initialize new record for model, load data and submit at end
@@ -117,7 +152,8 @@ func OpenChatSocket(selectedModel *string, c echo.Context, app *pocketbase.Pocke
 			})
 			if err := modelForm.Submit(); err != nil {
 				fmt.Printf("Failed to initialize model message in chat DB: %v\n", err)
-				return err
+				handleChatError(err, ws, htmxMsg.ThreadId, app)
+				continue
 			}
 
 			// send the initial response skeleton
@@ -133,16 +169,14 @@ func OpenChatSocket(selectedModel *string, c echo.Context, app *pocketbase.Pocke
 			var htmlBuf bytes.Buffer
 			err = chatComponent.Render(context.Background(), &htmlBuf)
 			if err != nil {
-				fmt.Println("templ render error")
-				fmt.Println(err)
-				return err
+				handleChatError(err, ws, htmxMsg.ThreadId, app)
+				continue
 			}
 			htmlStr := htmlBuf.String()
 			err = ws.WriteMessage(websocket.TextMessage, []byte(htmlStr))
 			if err != nil {
-				fmt.Println("socket write failure")
-				fmt.Println(err)
-				return err
+				handleChatError(err, ws, htmxMsg.ThreadId, app)
+				continue
 			}
 
 			// get all chats from thread, create ChatCompletionMessage so model has context
@@ -188,7 +222,8 @@ func OpenChatSocket(selectedModel *string, c echo.Context, app *pocketbase.Pocke
 			stream, err := chatgptClient.CreateChatCompletionStream(context.Background(), req)
 			if err != nil {
 				fmt.Printf("ChatCompletionStream error: %v\n", err)
-				return err
+				handleChatError(err, ws, htmxMsg.ThreadId, app)
+				continue
 			}
 			defer stream.Close()
 
@@ -204,7 +239,8 @@ func OpenChatSocket(selectedModel *string, c echo.Context, app *pocketbase.Pocke
 
 				if err != nil {
 					fmt.Printf("\nStream error: %v\n", err)
-					return err
+					handleChatError(err, ws, htmxMsg.ThreadId, app)
+					continue
 				}
 
 				fullResponse += response.Choices[0].Delta.Content
@@ -217,7 +253,8 @@ func OpenChatSocket(selectedModel *string, c echo.Context, app *pocketbase.Pocke
 				if err != nil {
 					fmt.Println("templ stream chunk render error")
 					fmt.Println(err)
-					return err
+					handleChatError(err, ws, htmxMsg.ThreadId, app)
+					continue
 				}
 				htmlStr := htmlBuf.String()
 				fmt.Println(htmlStr)
@@ -225,7 +262,8 @@ func OpenChatSocket(selectedModel *string, c echo.Context, app *pocketbase.Pocke
 				if err != nil {
 					fmt.Println("socket write failure")
 					fmt.Println(err)
-					return err
+					handleChatError(err, ws, htmxMsg.ThreadId, app)
+					continue
 				}
 			}
 
@@ -239,13 +277,15 @@ func OpenChatSocket(selectedModel *string, c echo.Context, app *pocketbase.Pocke
 
 			if err := modelForm.Submit(); err != nil {
 				fmt.Printf("Failed to submit model message to chat DB: %v\n", err)
-				return err
+				handleChatError(err, ws, htmxMsg.ThreadId, app)
+				continue
 			}
 
 			threadRecord, err := app.Dao().FindRecordById("chat_meta", htmxMsg.ThreadId)
 			if err != nil {
 				fmt.Printf("Error reading thread metadata: %v\n", err)
-				return err
+				handleChatError(err, ws, htmxMsg.ThreadId, app)
+				continue
 			}
 
 			lastMessageTime := types.NowDateTime()
@@ -258,7 +298,8 @@ func OpenChatSocket(selectedModel *string, c echo.Context, app *pocketbase.Pocke
 			if err != nil {
 				fmt.Println("templ stream chunk render error")
 				fmt.Println(err)
-				return err
+				handleChatError(err, ws, htmxMsg.ThreadId, app)
+				continue
 			}
 			lastMessageTimeStr := lastMessageTimeBuf.String()
 			fmt.Println(lastMessageTimeStr)
@@ -266,14 +307,16 @@ func OpenChatSocket(selectedModel *string, c echo.Context, app *pocketbase.Pocke
 			if err != nil {
 				fmt.Println("socket write failure")
 				fmt.Println(err)
-				return err
+				handleChatError(err, ws, htmxMsg.ThreadId, app)
+				continue
 			}
 
 			threadRecord.Set("last_message_timestamp", types.NowDateTime())
 			threadRecord.Set("last_message", fullResponse[0:10])
 			if err := app.Dao().SaveRecord(threadRecord); err != nil {
 				fmt.Printf("Error updating thread metadata: %v\n", err)
-				return err
+				handleChatError(err, ws, htmxMsg.ThreadId, app)
+				continue
 			}
 		}
 	}
